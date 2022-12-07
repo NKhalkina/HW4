425da363b480922b70fd344207964dbde1c28142
Khalkina <tashik2010@mail.ru>
2022-12-08 00:37:49 +0300

    Решение задачи №27

---
+++ exe27/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Введите корректное число");
+        }
+    }
+    return result;
+}
+
+int number = GetNumber("Введите число");
+int summ = 0;
+{
+    
+    while (number > 0)
+    {
+    summ = summ + number % 10;
+    number = number / 10;
+    }
+}
+Console.WriteLine(summ);
+
+
+
+
+
